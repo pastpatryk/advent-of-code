47742ca6f46b862034869c16713ac564b01f547b
Patryk Pastewski <past.patryk@gmail.com>
2020-12-14 14:03:07 +0100

    Day 12 | ⭐⭐

--- day_12/main.go
+++ day_12/main.go
@@ -11,23 +11,38 @@ import (
 
 // Ship represents position and direction of a ship
 type Ship struct {
-	x      int
-	y      int
-	facing int
+	x        int
+	y        int
+	waypoint [2]int
 }
 
-// Move changes ship's position
-func (s *Ship) Move(dir byte, val int) {
+// MoveWaypoint changes ship's waypoint
+func (s *Ship) MoveWaypoint(dir byte, val int) {
 	switch dir {
 	case 'N':
-		s.y -= val
+		s.waypoint[1] -= val
 	case 'E':
-		s.x += val
+		s.waypoint[0] += val
 	case 'S':
-		s.y += val
+		s.waypoint[1] += val
 	case 'W':
-		s.x -= val
+		s.waypoint[0] -= val
 	}
+}
+
+// RotateWaypoint rotates waypoint by a given degree
+func (s *Ship) RotateWaypoint(val int) {
+	deg := float64(val) * math.Pi / 180
+	x := float64(s.waypoint[0])
+	y := float64(s.waypoint[1])
+	s.waypoint[0] = int(math.Round(x*math.Cos(deg) - y*math.Sin(deg)))
+	s.waypoint[1] = int(math.Round(x*math.Sin(deg) + y*math.Cos(deg)))
+}
+
+// Move moves ship to waypoiint given amount of times
+func (s *Ship) Move(val int) {
+	s.x += s.waypoint[0] * val
+	s.y += s.waypoint[1] * val
 }
 
 // Instruction ...
@@ -39,16 +54,15 @@ type Instruction struct {
 // Execute applies instructoni to ship
 func (i *Instruction) Execute(ship *Ship) {
 	fmt.Printf("Executing %c %d on %v\n", i.op, i.val, ship)
-	dirs := []byte{'N', 'E', 'S', 'W'}
 	switch i.op {
 	case 'R':
-		ship.facing = (ship.facing + (i.val / 90) + len(dirs)) % len(dirs)
+		ship.RotateWaypoint(i.val)
 	case 'L':
-		ship.facing = (ship.facing - (i.val / 90) + len(dirs)) % len(dirs)
+		ship.RotateWaypoint(-i.val)
 	case 'F':
-		ship.Move(dirs[ship.facing], i.val)
+		ship.Move(i.val)
 	default:
-		ship.Move(i.op, i.val)
+		ship.MoveWaypoint(i.op, i.val)
 	}
 }
 
@@ -66,11 +80,11 @@ func main() {
 	instructions, err := readInstructions(file)
 	check(err)
 
-	ship := Ship{facing: 1}
+	ship := Ship{waypoint: [2]int{10, -1}}
 	for _, inst := range instructions {
 		inst.Execute(&ship)
 	}
-	fmt.Printf("Ship position: %v (%f)\n", ship, math.Abs(float64(ship.x))+math.Abs(float64(ship.y)))
+	fmt.Printf("Ship position: %v (%0.f)\n", ship, math.Abs(float64(ship.x))+math.Abs(float64(ship.y)))
 }
 
 func readInstructions(reader io.Reader) ([]Instruction, error) {
